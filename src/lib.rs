ee47eda7c35bf7d4aa2fe8f0dfa9548d498c0da1
Joshua Abraham <sinisterpatrician@gmail.com>
2020-05-22 18:20:55 -0400

    Trace sleep syscall

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,70 @@
+mod errors;
+
+use nix::sys::ptrace;
+use nix::sys::wait::waitpid;
+use nix::unistd::Pid;
+
+use crate::errors::*;
+
+pub fn trace_read(pid: Pid) -> Result<()> {
+    ptrace::attach(pid)?;
+    waitpid(pid, None)?;
+    eprintln!("pid attached !");
+
+    loop {
+        // Enter syscall
+        ptrace::syscall(pid, None)?;
+        waitpid(pid, None)?;
+        let regs = ptrace::getregs(pid)?;
+        eprintln!(" = {}", regs.rax);
+
+        //if syscall_num == libc::SYS_exit_group.try_into().unwrap() {
+        //    eprintln!("{:?} exiting", pid);
+        //    return Ok(());
+        //}
+
+        // Run syscall and stop on exit
+        match ptrace::syscall(pid, None) {
+            Err(e) => {
+                eprintln!("Err: {:?}", e);
+                return Ok(());
+            },
+            _ => (),
+        };
+        waitpid(pid, None)?;
+        let regs = ptrace::getregs(pid)?;
+        eprint!("{}({}, {}, {}, {}, {}, {})", 
+                    regs.orig_rax, regs.rdi, regs.rsi, regs.rdx, regs.r10, regs.r8, regs.r9);
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use nix::sys::signal::{kill, Signal};
+    use nix::unistd::execv;
+    use nix::unistd::{fork, ForkResult};
+    use std::ffi::CString;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_trace_sleep() {
+
+        match fork() {
+            Ok(ForkResult::Parent { child, .. }) => {
+                let res = trace_read(child);
+                //eprintln!("{:?}", res);
+                kill(child, Some(Signal::SIGKILL)).unwrap();
+            }
+            Ok(ForkResult::Child) => {
+                execv(
+                    &CString::new("./tests/test_prog").unwrap(),
+                    &[
+                        &CString::new("./tests/test_prog").unwrap(),
+                    ],
+                )
+                .unwrap();
+            }
+            Err(_) => panic!("Fork failed"),
+        }
     }
 }
---
+++ tests/test_prog.c
@@ -0,0 +1,8 @@
+int main(void)
+{
+    while (1) {
+        puts("sleeping...");
+        sleep(1);
+    }
+    return 0;
+}
