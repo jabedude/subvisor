7fc0a174ac6a706890a01849931cb28d42c9c1a8
Joshua Abraham <sinisterpatrician@gmail.com>
2020-05-22 20:56:36 -0400

    WIP: sysint trace child after setting TRACEME before execve

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,30 @@
 mod errors;
 
+use libc::user_regs_struct;
+use log::*;
 use nix::sys::ptrace;
 use nix::sys::wait::waitpid;
 use nix::unistd::Pid;
 
 use crate::errors::*;
 
+/// Replacement for nanosleep(2).
+/// Prototype: int nanosleep(const struct timespec *req, struct timespec *rem);
+fn nanosleep_hook(regs: user_regs_struct) -> Result<user_regs_struct> {
+    Ok(regs)
+}
+
 pub fn trace_read(pid: Pid) -> Result<()> {
     ptrace::attach(pid)?;
     waitpid(pid, None)?;
-    eprintln!("pid attached !");
+    debug!("pid attached !");
 
     loop {
         // Enter syscall
         ptrace::syscall(pid, None)?;
         waitpid(pid, None)?;
         let regs = ptrace::getregs(pid)?;
-        eprintln!(" = {}", regs.rax);
+        let return_val = regs.rax;
 
         //if syscall_num == libc::SYS_exit_group.try_into().unwrap() {
         //    eprintln!("{:?} exiting", pid);
@@ -26,16 +34,40 @@ pub fn trace_read(pid: Pid) -> Result<()> {
         // Run syscall and stop on exit
         match ptrace::syscall(pid, None) {
             Err(e) => {
-                eprintln!("Err: {:?}", e);
+                error!("Err: {:?}", e);
                 return Ok(());
             },
             _ => (),
         };
+
         waitpid(pid, None)?;
         let regs = ptrace::getregs(pid)?;
-        eprint!("{}({}, {}, {}, {}, {}, {})", 
-                    regs.orig_rax, regs.rdi, regs.rsi, regs.rdx, regs.r10, regs.r8, regs.r9);
+        debug!("{}({}, {}, {}, {}, {}, {})={}",
+                    regs.orig_rax, regs.rdi, regs.rsi,
+                    regs.rdx, regs.r10, regs.r8, regs.r9,
+                    return_val);
     }
+}
+
+fn sysint(pid: Pid) -> Result<()> {
+    waitpid(pid, None)?;
+    ptrace::setoptions(pid, ptrace::Options::PTRACE_O_EXITKILL)?;
+
+    loop {
+        ptrace::syscall(pid, None)?;
+        waitpid(pid, None)?;
+        let regs = ptrace::getregs(pid)?;
+
+        debug!("Syscall: {}", regs.orig_rax);
+
+        ptrace::syscall(pid, None)?;
+        waitpid(pid, None)?;
+
+        let regs = ptrace::getregs(pid)?;
+        debug!("Syscall ret: {}", regs.rax);
+    }
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -47,7 +79,33 @@ mod tests {
     use std::ffi::CString;
 
     #[test]
+    fn test_sysint() {
+        env_logger::init();
+
+        match fork() {
+            Ok(ForkResult::Parent { child, .. }) => {
+                let res = sysint(child);
+                //eprintln!("{:?}", res);
+                kill(child, Some(Signal::SIGKILL)).unwrap();
+            }
+            Ok(ForkResult::Child) => {
+                nix::sys::ptrace::traceme().expect("traceme");
+                //kill(nix::unistd::getpid(), Some(Signal::SIGSTOP)).unwrap();
+                execv(
+                    &CString::new("./tests/test_prog").unwrap(),
+                    &[
+                        &CString::new("./tests/test_prog").unwrap(),
+                    ],
+                )
+                .unwrap();
+            }
+            Err(_) => panic!("Fork failed"),
+        }
+    }
+
+    #[test]
     fn test_trace_sleep() {
+        env_logger::init();
 
         match fork() {
             Ok(ForkResult::Parent { child, .. }) => {
